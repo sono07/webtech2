1d95aa0d48099a5052421d1816a6e95461ce49ad
sono07 <noncsi.0071@gmail.com>
2020-07-22 09:32:07 +0200

    Add existing file

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -13,3 +13,63 @@ routes.get("/readBooks", (req, resp) => {
         resp.status(400).contentType("application/json").send({"error": error});
     });
 });
+
+
+routes.post("/readBooksForTitle", (req, resp) => {
+    if(req.body["title"] === undefined) {
+        resp.status(400).contentType("application/json").send({"error": "title must be defined"});
+        return;
+    }
+
+    bookService.readBooksForTitle(req.body["title"], (books) => {
+        resp.status(200).contentType("application/json").send({"books": books});
+    }, (error) => {
+        resp.status(400).contentType("application/json").send({"error": error});
+    });
+});
+
+routes.post("/readBooksForAuthor", (req, resp) => {
+    if(req.body["author"] === undefined) {
+        resp.status(400).contentType("application/json").send({"error": "author must be defined"});
+        return;
+    }
+
+    bookService.readBooksForAuthor(req.body["author"], (books) => {
+        resp.status(200).contentType("application/json").send({"books": books});
+    }, (error) => {
+        resp.status(400).contentType("application/json").send({"error": error});
+    });
+});
+
+routes.post("/readBookForISBN", (req, resp) => {
+    if(req.body["isbn"] === undefined) {
+        resp.status(400).contentType("application/json").send({"error": "ISBN must be defined"});
+        return;
+    }
+	
+    bookService.readBookForISBN(req.body["isbn"], (book) => {
+        resp.status(200).contentType("application/json").send({"book": book});
+    }, (error) => {
+        resp.status(400).contentType("application/json").send({"error": error});
+    });
+});
+
+routes.post("/createBook", (req, resp) => {
+    let book;
+    try {
+        book = new Book.BookFromJson(req.body["book"]);
+    } catch (error) {
+        resp.status(400).contentType("application/json").send({"error": error});
+        return;
+    }
+
+    bookService.createBook(book, (isbn) => {
+        resp.status(200).contentType("application/json").send({"isbn": isbn});
+    }, (error) => {
+        resp.status(400).contentType("application/json").send({"error": error});
+    });
+});
+
+module.exports = {
+    routes: routes
+};
